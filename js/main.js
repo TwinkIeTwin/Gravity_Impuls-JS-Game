63b3ddeef84956b2db9b83dd2ad5f649c0a63931
TwinkIeTwin <matyuha4@yandex.ru>
2020-02-15 14:47:19 +0300

    init: project structure

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    lastTime = now;
+};
